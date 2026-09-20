e311d3bbb9e3a5390a189e2c059eba70ffd55834
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2026-09-20 04:54:37 +0300

    fix: revert changes into config.clj and merge it with component style features

--- src/core/config.clj
+++ src/core/config.clj
@@ -16,3 +16,22 @@
     (-> cfg
         (assoc-in [:db :username] (get-in sensitive [:db :username]))
         (assoc-in [:db :password] (get-in sensitive [:db :secret])))))
+
+(defn load-config
+  "Load common properties from resources."
+  []
+  (try
+    (edn/read-string (slurp "resources/config.edn"))
+    (catch FileNotFoundException _
+      (throw (ex-info "FATAL: resources/config.edn not found! Application cannot start." {})))))
+
+(defn load-sensitive-config
+  "Load sensitive properties from resources."
+  []
+  (try
+    (edn/read-string (slurp "resources/config.sensitive.edn"))
+    (catch FileNotFoundException _
+      (throw (ex-info "FATAL: resources/config.sensitive.edn not found! Application cannot start." {})))))
+
+(defonce config (load-config))
+(defonce sensitive-config (load-sensitive-config))
