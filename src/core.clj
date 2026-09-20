f2e06685ff21faaded276e3ddeb5fcd2d986ecd3
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2026-09-20 04:40:33 +0300

    fix: fixed core.clj code style & requires

--- src/core.clj
+++ src/core.clj
@@ -1,25 +1,25 @@
 (ns core
   (:require [com.stuartsierra.component :as component]
             [core.config :refer [fetch-config]]
-            [components.datasource :refer [datasource-component]]
+            [components.db.datasource :refer [datasource-component]]
             [components.server.component :refer [http-server-component]])
   (:gen-class))
 
 (defn create-system []
   (let [config (fetch-config)]
     (component/system-map
-      :datasource
-      (datasource-component config)
+     :datasource
+     (datasource-component config)
 
-      :http-server
-      (component/using
-        (http-server-component config)
-        [:datasource]))))
+     :http-server
+     (component/using
+      (http-server-component config)
+      [:datasource]))))
 
 (defn -main [& _args]
   (let [system (-> (create-system)
                    (component/start))]
     (.addShutdownHook
-      (Runtime/getRuntime)
-      (Thread. #(component/stop system)))
+     (Runtime/getRuntime)
+     (Thread. #(component/stop system)))
     (println "BranchYard started.")))
